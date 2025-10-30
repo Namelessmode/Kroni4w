d196fa06a9f30ae2cdc9eed57f89cc2f2dbb4f3c
Kroni4w <121424993+Namelessmode@users.noreply.github.com>
2025-10-30 14:45:48 +0600

    Update install.sh

--- Kroni-Extras/Kroni-Scripts/install.sh
+++ Kroni-Extras/Kroni-Scripts/install.sh
@@ -4,7 +4,9 @@ set -e
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
 #Install AUR Packages
-yay -Syu --needed - < pkgs.sh
-yay -S --needed - < thinkpad-440.sh
+yay -Syu --needed - < ~/Kroni4w/Kroni-Extras/Kroni-Scripts/pkgs.sh
+mv ~/Kroni4w/Kroni-Extras/* ~/
+mv ~/Kroni4w/* ~/.config/
+~/Kroni-Scripts/services.sh
 
 
